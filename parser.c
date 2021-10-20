a15e263a94a32b48e3585e77d6f948a3fbc072b3
z80jon <z80jon_laptop>
2021-10-19 23:38:44 -0500

    Parser works, but left to right only

--- parser.c
+++ parser.c
@@ -10,11 +10,12 @@ uint8_t parser(char* text, uint16_t* returnValue) {
         while(!parser_internal__isEndOfToken(text[textIndex])) {
             token[tokenIndex++] = text[textIndex++];
         }
+        printf("\n[Parser]: Stopped looking for new token input at character '%c'",text[textIndex]);
 
         //2) if in a math state (initial or add/sub/mult), perform the action on
         if(tokenIndex > 0) {
             if(parser_getValueOfToken(token, &val2)) {
-                printf("\n[Parser]: [FATAL ERROR]: Unable to resolve meaning of \"%s\"",token);
+                printf("\n[Parser]: [ERROR]: Unable to resolve meaning of \"%s\"",token);
                 return 1;
             }
                 
@@ -31,7 +32,7 @@ uint8_t parser(char* text, uint16_t* returnValue) {
                 
                 case PARSERSTATE__SUBTRACT:
                     if(val2 > val1) {
-                        printf("\n[Parser]: [FATAL ERROR]: Subtraction below zero");
+                        printf("\n[Parser]: [ERROR]: Subtraction below zero");
                         return 1;
                     }
                     val1 -= val2;
@@ -39,7 +40,7 @@ uint8_t parser(char* text, uint16_t* returnValue) {
                     break;
                 
                 case PARSERSTATE__MULTIPLY:
-                    //TODO
+                    val1 *= val2;
                     break;
                 
                 case PARSERSTATE__INDEX:
@@ -47,7 +48,7 @@ uint8_t parser(char* text, uint16_t* returnValue) {
                     break;
 
                 case PARSERSTATE__EXPECTING_OPERATOR:
-                    printf("\n[Parser]: [FATAL ERROR]: Was expecting another operator and got \"%s\".",token);
+                    printf("\n[Parser]: [ERROR]: Was expecting another operator and got \"%s\".",token);
                     break;
             }
 
@@ -55,42 +56,117 @@ uint8_t parser(char* text, uint16_t* returnValue) {
 
         switch(text[textIndex]) {
             case '+':
-                //TODO
+                state = PARSERSTATE__ADD;
                 break;
+
             case '-':
-                //TODO
+                state = PARSERSTATE__SUBTRACT;
                 break;
+
             case '*':
-                //TODO
+                state = PARSERSTATE__MULTIPLY;
                 break;
+
             case '[':
                 if(library_getVariableAddress(token,&scratchpad)!=LIBRARY_STATUS__NO_ERRORS) {
-                    printf("\n[Parser]: [Error]: Indexing attempted on non-variable \"%s\"",token);
+                    printf("\n[Parser]: [ERROR]: Indexing attempted on non-variable \"%s\"",token);
                     return 1;
                 }
 
-                char* buff = (char*)calloc(strlen(text[textIndex]+1)+1,sizeof(char));
-                strcpy(buff, &(text[textIndex+1]));
-                buff = strtok(buff, ']');
+                textIndex++;//Advance past the first '('
 
-                textIndex += strlen(buff)+2;//Shift pointer past everything inside the brackets
+                //Find the index of the matching ] and set it to null terminator
+                uint8_t matching_bracket_index;
+                uint8_t closing_brackets_remaining = 1;
+                for(matching_bracket_index = textIndex; matching_bracket_index < strlen(text) && closing_brackets_remaining > 0; matching_bracket_index++) {
+                    if(text[matching_bracket_index] == ']')
+                        closing_brackets_remaining--;
+                    if(text[matching_bracket_index] == '[')
+                        closing_brackets_remaining++;
+                }
+                matching_bracket_index--;//Because FOR increments before checking, matching_bracket_index will overshoot the matching ] by 1.
+                if(text[matching_bracket_index] == '\0') {
+                    printf("\n[Parser]: [ERROR]: Ran into [ with no matching ] when parsing text \"%s\"",text);
+                    return 1;
+                }
+                text[matching_bracket_index] = '\0';
 
+                //Copy over the text within the matching [ ]
+                char* buff = (char*)calloc(strlen(&(text[textIndex])),sizeof(char));
+                strcpy(buff, &(text[textIndex]));
+
+                text[matching_bracket_index] = ']';//Putting it back to this will make debug outputs look a bit cleaner.
+
+                textIndex = matching_bracket_index;//Shift pointer to closing bracket
                 if(parser(buff, &val2) != 0) {
-                    printf("\n[Parser]: Failed when parsing text between brackets \"%s\"",buff);
+                    printf("\n[Parser]: [ERROR]: Failed when parsing text between brackets \"%s\"",buff);
                     return 1;
                 }
 
                 val1 += val2;
+                state = PARSERSTATE__EXPECTING_OPERATOR;
                 break;
+
             case '(':
-                //TODO
+                textIndex++;//Advance past the first '('
+
+                //Find the index of the matching ) and set it to null terminator
+                uint8_t matching_parentheses_index;
+                uint8_t closing_parentheses_remaining = 1;
+                for(matching_parentheses_index = textIndex; matching_parentheses_index < strlen(text) && closing_parentheses_remaining > 0; matching_parentheses_index++) {
+                    if(text[matching_parentheses_index] == ')')
+                        closing_parentheses_remaining--;
+                    if(text[matching_parentheses_index] == '(')
+                        closing_parentheses_remaining++;
+                }
+
+                matching_parentheses_index--;//Because FOR increments before checking, matching_parentheses_index will overshoot the matching ] by 1.
+                if(text[matching_parentheses_index] == '\0') {
+                    printf("\n[Parser]: [ERROR]: Ran into ( with no matching ) when parsing text \"%s\"",text);
+                    return 1;
+                }
+                text[matching_parentheses_index] = '\0';
+
+                //Copy over the text within the matching [ ]
+                buff = (char*)calloc(strlen(&(text[textIndex])),sizeof(char));
+                strcpy(buff, &(text[textIndex]));
+
+                text[matching_parentheses_index] = ')';//Putting it back to this will make debug outputs look a bit cleaner.
+
+                textIndex = matching_parentheses_index;//Shift pointer to closing parentheses
+
+                if(parser(buff, &val2) != 0) {
+                    printf("\n[Parser]: [ERROR]: Failed when parsing text between parentheses \"%s\"",buff);
+                    return 1;
+                }
+
+                switch(state) {
+                    case PARSERSTATE__INITIAL_NUMBER:
+                        val1 = val2;
+                        break;
+                    case PARSERSTATE__ADD:
+                        val1 += val2;
+                        break;
+                    case PARSERSTATE__MULTIPLY:
+                        printf("Multiplying\n");
+                        val1 *= val2;
+                        break;
+                    case PARSERSTATE__SUBTRACT:
+                        if(val2 > val1) {
+                        printf("\n[Parser]: [ERROR]: Subtraction below zero");
+                        return 1;
+                        }
+                        val1 -= val2;
+                        break;
+                }
+                state = PARSERSTATE__EXPECTING_OPERATOR;
                 break;
             
             case '\0':
                 break;
             
             default:
-                printf("\n[Parser]: [Error]: Encountered unexpected symbol '%c'",text[textIndex]);
+                printf("\n[Parser]: [ERROR]: Encountered unexpected symbol '%c'",text[textIndex]);
                 return 1;
         }
 
@@ -98,18 +174,12 @@ uint8_t parser(char* text, uint16_t* returnValue) {
             token[i] = 0;
         }
         tokenIndex = 0;
+        textIndex++;
         
     }
    
-        
-
-    printf("\n[Parser]: Translated '%s' to: %d/0x%X.",text, val1, val1);
-
-    //    token = strtok(NULL," ");
-    //    firstToken = 0;
-    //}
-
-//    free(token);
+    free(token);
+    printf("\n[Parser]: Translated \"%s\" to: %d/0x%X.",text, val1, val1);
 
     *returnValue = val1;
     return 0;
@@ -134,12 +204,12 @@ uint8_t parser_getValueOfToken(char* text, uint16_t* returnValue) {
     else if(library_getLabelAddress(text, returnValue) != LIBRARY_STATUS__NAME_NOT_FOUND);
 
     else {
-        printf("\n\n[FATAL ERROR]: [Parser]: Unable to resolve meaning of token '%s'",text);
+        printf("\n[Parser]: [ERROR]: Unable to resolve meaning of token '%s'",text);
         return 1;
     }
     return 0;
 }
 
 uint8_t parser_internal__isEndOfToken(char c) {
-   return (c == ' ') || (c == '[') || (c == ']') || (c == '*') || (c == '+') || (c == '-') || (c == '\0');     
+   return (c == ' ') || (c == '[') || (c == ']') || (c == '*') || (c == '+') || (c == '-') || (c == '\0') || (c=='(') || (c==')');
 }
